4b66e0c46ea088a11d1a3713b54480f775856fb3
antony.sl <antony.g.s.lopez@gmail.com>
2026-05-30 00:34:39 -0500

    chore: configurar variables de entorno y pool de PostgreSQL

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,5 +1,5 @@
 import z from "zod";
-
+//creamos el esquema de validacion de variables de entorno
 const envSchema = z.object({
   PORT: z.string().transform((val) => Number(val)),
   DB_HOST: z.string(),
@@ -8,7 +8,7 @@ const envSchema = z.object({
   DB_PASSWORD: z.string(),
   DB_NAME: z.string(),
 });
-
+// se usa safeParse para evitar que el proceso se detenga
 const parsed = envSchema.safeParse(process.env);
 // Si no se parsea correctamente, salir
 if(!parsed.success){
---
+++ backend/src/config/postgres.ts
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+import env from "../config/env.js";
+
+const pool = new Pool({
+    host: env.DB_HOST,
+    port: env.DB_PORT,
+    user: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME
+});
+
+export default pool;
