8fe139fdcf3634fd822b3cbc8268554f03c305a4
antony.sl <antony.g.s.lopez@gmail.com>
2026-06-24 20:21:42 -0500

    feat(security): agregar helmet y actualizar documentacion

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import errorGlobalMiddleware from './middleWare/global/errorGlobal.middleware.js';
 import verificarToken from './middleWare/global/verificarToken.middleware.js';
 import authRoutes from "./routes/auth.routes.js";
+import helmet from 'helmet';
 
 import routes from './routes/index.js';
 
 // al usar typescript, es necesario especificar el tipo de la variable: en este caso express.Application
 const app: express.Application = express();
-
+// helmet para headers de seguridad
+app.use(helmet());
 // middleware base para parsear el body de las peticiones http
 app.use(express.json());
 app.use("/auth", authRoutes);
